d3f0b142326369eb231ae1e88fe77af15f6ed003
nestand <andrey.nesterov@laposte.net>
2021-10-20 11:47:11 +0200

    data input class added with constructor. TEST OK! -> new object was successfully created

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,18 @@
 "use strict";
-//code goes here!
+//make the form visible
+class DataInput {
+    constructor() {
+        //  ! in the end to remove "null" error
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedData = document.importNode(this.templateElement.content, true);
+        this.element = importedData.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+//test 
+const firstInput = new DataInput;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-//code goes here!
+//make the form visible
+
+class DataInput {
+    // tsconfig lib changed to work with DOM Elements
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        //  ! in the end to remove "null" error
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedData = document.importNode(this.templateElement.content, true);
+        this.element = importedData.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+//test 
+const firstInput = new DataInput;

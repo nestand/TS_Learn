da2643ff3e15ad3b85f3a855aad781b42bdbd539
nestand <andrey.nesterov@laposte.net>
2021-10-20 13:57:18 +0200

    constructor modified, inputting data error fixed, css improved

--- dist/app.js
+++ dist/app.js
@@ -7,10 +7,26 @@ class DataInput {
         this.hostElement = document.getElementById('app');
         const importedData = document.importNode(this.templateElement.content, true);
         this.element = importedData.firstElementChild;
+        // css style improvement
+        this.element.id = 'user-input';
+        this.configure();
         this.attach();
+        //titleInput element goes here
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+    //adding an event listener
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+    //fixing Expected 2-3 arguments, but got 1.ts(2554) lib.dom.d.ts(6627, 68): An argument for 'listener' was not provided with a new method to bind it.
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
     }
 }
 //test 
--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@
 
 class DataInput {
     // tsconfig lib changed to work with DOM Elements
+    //getting the form
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    
+    //input data in the form
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
 
     constructor() {
         //  ! in the end to remove "null" error
@@ -13,13 +20,36 @@ class DataInput {
 
         const importedData = document.importNode(this.templateElement.content, true);
         this.element = importedData.firstElementChild as HTMLFormElement;
+        
+        // css style improvement
+        this.element.id = 'user-input';
+        this.configure();
         this.attach();
+
+        //titleInput element goes here
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
     }
 
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
+
+    //adding an event listener
+    private configure(){
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+
+    }
+
+    //fixing Expected 2-3 arguments, but got 1.ts(2554) lib.dom.d.ts(6627, 68): An argument for 'listener' was not provided with a new method to bind it.
+    private submitHandler(event: Event) {
+event.preventDefault();
+console.log(this.titleInputElement.value);
+    }
 }
+
 
 //test 
 const firstInput = new DataInput;
